242d54dd04576a110a4d26c46b94fa51e1905524
DamiT123 <C24344666@mytudublin.ie>
2026-09-10 11:45:51 +0900

    Add environment configuration to run.py

--- Beauty Page/run.py
+++ Beauty Page/run.py
@@ -1,7 +1,8 @@
 # run.py - Entry point to start the Flask application
+import os
 from app import create_app
 
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)), debug=True)
